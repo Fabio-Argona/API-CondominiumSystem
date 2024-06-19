8b3228581d355f0006a198dea6a74da3a5ec16c2
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-06-19 15:39:07 -0300

    Start Project Spring Boot

---
+++ src/main/java/com/condominium/api_condominium/ApiCondominiumApplication.java
@@ -0,0 +1,13 @@
+package com.condominium.api_condominium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCondominiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCondominiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/condominium/api_condominium/ApiCondominiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condominium.api_condominium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCondominiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
