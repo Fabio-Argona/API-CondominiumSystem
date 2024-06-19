d5597813f27cd5ab386e6b9cc3803e34fcd1ddb9
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-06-19 16:33:48 -0300

    Create model resident

---
+++ api_resident/src/main/java/com/condominium/resident/controller/ResidentController.java
@@ -0,0 +1,11 @@
+package com.condominium.resident.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/residents")
+public class ResidentController {
+    
+
+}
