988c0d1058640ccdfbb3f7353d207302ecdcb923
Fabio Argona <128233610+Fabio-Argona@users.noreply.github.com>
2024-06-25 09:22:44 -0300

    Update resident-form

--- site_condominium/src/app/app.routes.ts
+++ site_condominium/src/app/app.routes.ts
@@ -5,45 +5,16 @@ import { LoginComponent } from './pages/login/login.component';
 import { OwnersComponent } from './administrator/owners/owners.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResidentDataComponent } from './administrator/resident-data/resident-data.component';
-import { ResidentFormComponent } from './administrator/administrator/resident-form/resident-form.component';
+import { ResidentFormComponent } from './administrator/resident-form/resident-form.component';
 
 export const routes: Routes = [
 
-  {
-    path: '',
-    title: 'Home da Pagina',
-    component: HomeComponent,
-  },
-
-  {
-    path: 'resident',
-    component: ResidentComponent,
-  },
-
-  {
-    path: 'owner',
-    component: OwnersComponent,
-  },
-
-  {
-    path: 'resident_data',
-    component: ResidentDataComponent,
-  },
-
-  {
-    path: 'resident-form:id',
-    component: ResidentFormComponent,
-  },
-
-  {
-    path: 'login',
-    title: 'Faça seu login',
-    component: LoginComponent,
-  },
-
-  {
-    path: '**',
-    component: NotFoundComponent,
-  },
+  { path: '', component: HomeComponent },
+  { path: 'resident', component: ResidentComponent },
+  { path: 'owner', component: OwnersComponent },
+  { path: 'resident-form/:id', component: ResidentFormComponent },
+  { path: 'resident_data', component: ResidentDataComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NotFoundComponent },
 
 ];
--- site_condominium/src/app/pages/resident/service/resident.service.ts
+++ site_condominium/src/app/pages/resident/service/resident.service.ts
@@ -1,6 +1,9 @@
-import { Resident } from './../model/resident';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
+
+import { Resident } from '../../resident/model/resident';
+import { map } from 'rxjs';
+
 
 
 @Injectable({
@@ -8,28 +11,28 @@ import { Injectable, inject } from '@angular/core';
 })
 export class ResidentService {
 
-  private http = inject(HttpClient);
 
-  list(){
+  constructor(private http: HttpClient) {}
+
+  list() {
     return this.http.get<Resident[]>('http://localhost:9001/api/residents');
   }
 
-  get(id: string){
+  get(id: string) {
     return this.http.get<Resident>(`http://localhost:9001/api/residents/getId/${id}`);
   }
 
-  create(client: Resident ){
-    return this.http.post<Resident>('http://localhost:9001/api/residents', client);
+  create(resident: Resident) {
+    return this.http.post<Resident>('http://localhost:9001/api/residents', resident);
   }
 
-  update(id: string, client: Resident ){
-    return this.http.put<Resident>(`http://localhost:9001/api/residents/${id}`, client);
+  update(id: string, resident: Resident) {
+    return this.http.put<Resident>(`http://localhost:9001/api/residents/${id}`, resident);
   }
 
-  delete(id: string){
+  delete(id: string) {
     return this.http.delete<void>(`http://localhost:9001/api/residents/${id}`);
   }
 
+  
 }
-export { Resident };
-
